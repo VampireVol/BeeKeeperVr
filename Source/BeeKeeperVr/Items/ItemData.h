e4f9c12b1519c2ba9ef5d747ebc3ac9cf56f60d4
Sergey Balanin <sergeyb11@ya.ru>
2025-07-23 02:33:36 +0300

    Add items to inventory

---
+++ Source/BeeKeeperVr/Items/ItemData.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EItemType.h"
+#include "ItemData.generated.h"
+
+USTRUCT(BlueprintType)
+struct BEEKEEPERVR_API FItemData
+{
+  GENERATED_BODY()
+  UPROPERTY(EditAnywhere, BlueprintReadWrite)
+  EItemType Type;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite)
+  int Count;
+};
