b5c9aaf3348e533e357455d6d9b9a7773c2d08bf
Sergey Balanin <sergeyb11@ya.ru>
2025-07-31 02:43:16 +0300

    Create centrifuge icon and buildings recipe

---
+++ Source/BeeKeeperVr/Building/BuildingRecipeData.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../Items/ItemData.h"
+#include "Engine/DataTable.h"
+#include "BuildingRecipeData.generated.h"
+
+UENUM(BlueprintType)
+enum class EBuildingType : uint8
+{
+  None UMETA(DisplayName = "None"),
+  Beehive UMETA(DisplayName = "Beehive"),
+  Centrifuge UMETA(DisplayName = "Centrifuge"),
+  BeeHotel UMETA(DisplayName = "Bee Hotel"),
+  BeeAltar UMETA(DisplayName = "Bee Altar"),
+};
+
+USTRUCT(BlueprintType)
+struct BEEKEEPERVR_API FBuildingRecipeData : public FTableRowBase
+{
+  GENERATED_BODY()
+  UPROPERTY(EditAnywhere, BlueprintReadWrite)
+  EBuildingType Type;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite)
+  UTexture2D *Icon;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite)
+  TArray<FItemData> Cost;
+};
