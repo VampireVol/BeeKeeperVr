b9ee83492ae09d114972d1c433a368376295b9a4
Sergey Balanin <sergeyb11@ya.ru>
2025-10-11 03:18:54 +0300

    Add research data and display restoring altar stages title

---
+++ Source/BeeKeeperVr/Research/ResearchData.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../Items/ItemData.h"
+#include "../BeeGenetic.h"
+#include "Engine/DataTable.h"
+#include "TaskData.generated.h"
+
+USTRUCT(BlueprintType)
+struct BEEKEEPERVR_API FResearchData : public FTableRowBase
+{
+	GENERATED_BODY()
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FItemData> RequestItems;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 RequestDnaPoints;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText ResearchName;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UTexture2D *Icon;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText Caption;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText Description;
+};
+
